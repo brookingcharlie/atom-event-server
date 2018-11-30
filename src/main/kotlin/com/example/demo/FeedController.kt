a36c10ab3399fda40c017f513180f95aa3efba42
Charlie Brooking <cbrookin@thoughtworks.com>
2018-12-01 00:58:50 +1000

    generate five entries

--- src/main/kotlin/com/example/demo/FeedController.kt
+++ src/main/kotlin/com/example/demo/FeedController.kt
@@ -13,36 +13,39 @@ import java.util.*
 
 @Controller
 class FeedController {
+    private val baseHref = "http://localhost:8080/feed"
+
     @RequestMapping(method = [RequestMethod.GET], path = ["/feed"])
     @ResponseBody
     fun getFeed(out: OutputStream) {
-        val baseHref = "http://localhost:8080/feed"
+        val feed = buildFeed()
+        val writer = PrintWriter(out, false, Charsets.UTF_8)
+        WireFeedOutput().output(feed, writer)
+    }
 
-        val feed = Feed().apply {
+    private fun buildFeed(): Feed {
+        return Feed().apply {
             feedType = "atom_1.0"
             id = "${baseHref}/"
             title = "Demo Feed"
             otherLinks = listOf(Link().apply { rel = "self"; href = "${baseHref}/" })
             authors = listOf(Person().apply { name = "QTAC" })
             updated = Date()
+            entries = (1..5).map { buildEntry(it) }
         }
+    }
 
-        val entry = Entry().apply {
+    private fun buildEntry(entryId: Int): Entry {
+        return Entry().apply {
             title = "ROME v1.0"
-            id = "${baseHref}/entry-1"
+            id = "${baseHref}/${entryId}"
             updated = GregorianCalendar(2004, 5, 8).time
             contents = listOf(Content().apply {
                 type = "application/json"
                 value = base64("""{"title":"Test"}""")
             })
-            summary = Content().apply { type = "text/plain"; value = "Entry 1" }
+            summary = Content().apply { type = "text/plain"; value = "Entry ${entryId}" }
         }
-
-        feed.entries = listOf(entry)
-
-        val writer = PrintWriter(out, false, Charsets.UTF_8)
-        val output = WireFeedOutput()
-        output.output(feed, writer)
     }
 
     private fun base64(s: String) = Base64.getEncoder().encodeToString(s.toByteArray(Charsets.UTF_8))
