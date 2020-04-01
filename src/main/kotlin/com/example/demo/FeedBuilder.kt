9487d3e0ff601087b3d7416ed2259d876fbbceac
Charlie Brooking <cbrookin@thoughtworks.com>
2020-04-01 15:44:02 +1000

    experiment with multipart content

--- src/main/kotlin/com/example/demo/FeedBuilder.kt
+++ src/main/kotlin/com/example/demo/FeedBuilder.kt
@@ -1,5 +1,6 @@
 package com.example.demo
 
+import com.rometools.rome.feed.atom.Category
 import com.rometools.rome.feed.atom.Content
 import com.rometools.rome.feed.atom.Entry
 import com.rometools.rome.feed.atom.Feed
@@ -7,11 +8,15 @@ import com.rometools.rome.feed.atom.Link
 import com.rometools.rome.feed.atom.Person
 import org.springframework.data.domain.Page
 import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
 import java.time.Instant
 import java.time.OffsetDateTime
 import java.util.Base64
 import java.util.Date
 import java.util.UUID
+import javax.mail.internet.MimeBodyPart
+import javax.mail.internet.MimeMultipart
+
 
 @Service
 class FeedBuilder {
@@ -41,15 +46,39 @@ class FeedBuilder {
     }
 
     private fun buildEntry(event: Event): Entry {
+        val multipart = buildMultipartContent(event)
+        val multipartContentType: String = multipart.contentType.replace("\r\n\t", "")
+        val multipartContentBytes: ByteArray = ByteArrayOutputStream().also { multipart.writeTo(it) }.toByteArray()
+
         return Entry().apply {
             title = "Event ${event.id}"
             id = "urn:uuid:${event.id}"
             updated = date(event.timestamp)
-            contents = listOf(Content().apply { type = "application/json"; value = base64(event.payload) })
+            categories = listOf(Category().apply {
+                scheme = "http://example.org/categories/events/type"
+                term = "Something"
+            })
+            contents = listOf(Content().apply {
+                type = multipartContentType
+                value = base64(multipartContentBytes)
+            })
             summary = Content().apply { type = "text/plain"; value = "Event ID ${event.id}" }
         }
     }
 
+    private fun buildMultipartContent(event: Event): MimeMultipart {
+        val multipart = MimeMultipart("alternative")
+        multipart.addBodyPart(MimeBodyPart().apply {
+            setContent(event.payload, "text/plain; charset=utf-8")
+            setHeader("Content-Type", "application/vnd.example.something-v1+json; charset=utf-8")
+        })
+        multipart.addBodyPart(MimeBodyPart().apply {
+            setContent("""{"title":"Made up event to show second version"}""", "text/plain; charset=utf-8")
+            setHeader("Content-Type", "application/vnd.example.something-v2+json; charset=utf-8")
+        })
+        return multipart
+    }
+
     private fun date(offsetDateTime: OffsetDateTime) = Date.from(offsetDateTime.toInstant());
-    private fun base64(s: String) = Base64.getEncoder().encodeToString(s.toByteArray(Charsets.UTF_8))
+    private fun base64(s: ByteArray) = Base64.getEncoder().encodeToString(s)
 }
